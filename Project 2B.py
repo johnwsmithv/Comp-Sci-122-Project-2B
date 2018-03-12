0a4143c9a8bdecea768e4fd00ddce7c7798a394c
johnwsmithv <37300578+johnwsmithv@users.noreply.github.com>
2018-03-12 10:23:15 -0400

    Add files via upload

---
+++ Project 2B.py
@@ -0,0 +1,30 @@
+#Google API
+import urllib.request
+
+def GoogleMaps(origin,destination):
+    orgcity,orgstate= origin.split(',')
+    destcity,deststate = destination.split(',')
+    orgcity = orgcity.strip()
+    orgstate = orgstate.strip()
+    destcity = destcity.strip()
+    deststate = deststate.strip()
+    if " " in orgcity:
+        orgcity = orgcity.replace(' ','+')
+    elif " " in destcity:
+        destcity = destcity.replace(' ','+')
+    print(orgcity, orgstate)
+    print(destcity, deststate)
+    web_obj = urllib.request.urlopen(
+        "http://maps.googleapis.com/maps/api/distancematrix/json?origins=%s+%s&destinations=%s+%s&mode=driving&sensor"
+        "=false" % (orgcity,orgstate,destcity,deststate))
+    results_str = str(web_obj.read())
+    print(results_str)
+    web_obj.close()
+
+
+def main():
+    origincity = str(input("Enter your City and State of Origin, seperated by a comma: "))
+    destinationcity = str(input("Enter your Destination City and State, seperated by a comma: "))
+    GoogleMaps(origincity,destinationcity)
+
+main()
